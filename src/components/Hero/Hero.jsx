c9aa75a6cfc5aa4560790ae40c10070e89dc7a73
rn-mgl <reinb2ra@gmail.com>
2023-08-23 21:58:57 +0800

    added logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,11 +2,13 @@ import { Box } from "@mui/material";
 import Link from "next/link";
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
-import { BiChevronDown } from "react-icons/bi";
 
 export default function Hero() {
   return (
-    <Box className="tw-w-full tw-h-screen tw-p-10 tw-cstm-flex-col t:tw-p-20 l-s:tw-p-32 tw-justify-start ">
+    <Box
+      id="main"
+      className="tw-w-full tw-h-screen tw-p-10 tw-cstm-flex-col t:tw-p-20 l-s:tw-p-32 tw-justify-start "
+    >
       <Box className="tw-w-full tw-cstm-flex-col tw-gap-5 tw-items-start tw-my-auto">
         <Box className="tw-flex tw-flex-col tw-gap-2 tw-w-full t:tw-gap-5 ">
           <p className="tw-text-accntColor tw-text-sm tw-font-extralight t:tw-text-lg">
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { Box } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import logo from "@/public/global/logo.png";
+
 import { AiOutlineInfoCircle, AiOutlineMail, AiOutlineProject } from "react-icons/ai";
+import { Box } from "@mui/material";
 
 export default function Nav() {
   const [showNav, setShowNav] = React.useState(true);
@@ -30,7 +33,7 @@ export default function Nav() {
   return (
     <Box
       className={`tw-w-full tw-p-5 tw-fixed tw-top-0 tw-left-0
-                  tw-cstm-flex-row tw-gap-10 tw-text-xs tw-text-white tw-font-light t:tw-p-7 t:tw-text-sm
+                  tw-cstm-flex-row tw-gap-5 tw-text-xs tw-text-white tw-font-light t:tw-p-7 t:tw-text-sm
                   t:tw-gap-20 tw-z-20 tw-transition-all tw-backdrop-blur-md tw-shadow-md  
                   ${showNav ? "tw-translate-y-0" : "-tw-translate-y-full"} 
                   ${
@@ -39,6 +42,10 @@ export default function Nav() {
                       : "tw-backdrop-blur-md tw-shadow-md"
                   }`}
     >
+      <Link href="#main" className="tw-cstm-flex-row tw-gap-1 tw-mr-auto">
+        <Image src={logo} alt="logo" className="tw-w-6 t:tw-w-8" />
+      </Link>
+
       <Link href="#about" className="tw-cstm-flex-row tw-gap-1 t:tw-ml-auto">
         <Box>
           <AiOutlineInfoCircle className="tw-text-accntColor" />
